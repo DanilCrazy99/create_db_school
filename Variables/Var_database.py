21f6455384f0fc76d19948c5e82010ee2c049b09
Danil <volchara287@gmail.com>
2022-08-27 23:15:06 +0400

    Создал файл с переменными для создания сообщества(группы).

--- Variables/Var_database.py
+++ Variables/Var_database.py
@@ -1,8 +1,8 @@
-database = "postgres"
-user = ""
-password = ""
-host = "localhost"
+database = 'postgres'
+user = ''
+password = ''
+host = 'localhost'
 port = 5432
-schoolName = "schooltestbd"
-schoolPass = "87654321"
-class_school = "1A"
+schoolName = 'schooltestbd'
+schoolPass = '87654321'
+class_school = '1A'
