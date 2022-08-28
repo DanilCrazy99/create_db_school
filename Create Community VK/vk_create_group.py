8d9805c15ef4cef782ae4a68a311ff35d9c33eae
Danil <volchara287@gmail.com>
2022-08-28 09:09:25 +0400

    Создаю метод для получения списка групп Семен Семеныча

--- Create Community VK/vk_create_group.py
+++ Create Community VK/vk_create_group.py
@@ -1,5 +1,5 @@
 import vk_api
-from Variables.Var_community import vk_token, title, description, type
+from Variables.Var_community import vk_token, title, description, type, user_id
 
 session = vk_api.VkApi(token=vk_token)
 vk = session.get_api()
@@ -9,4 +9,9 @@ def create_group():
     vk.groups.create(title=title, description=description, type=type)
 
 
-create_group()
+def get_list_groups(var_user_id):
+    list_groups = vk.groups.get(user_id=var_user_id, filter='admin')
+    print(list_groups["count"], ["items"])
+
+
+get_list_groups(user_id)
--- Variables/Var_community.py
+++ Variables/Var_community.py
@@ -1,5 +1,8 @@
+from token import token
+vk_token = token
+user_id = '740705763'
+
 # Переменные для создания сообщества
-vk_token = ''
 title = 'Школьная группа'
 description = 'Группа для того чтобы написать базу данных школ'
 type = 'group'
