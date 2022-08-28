ad14cb0d7d6decec2d483884b457c37cdcf00f8f
Данил <112066653+DanilCrazy99@users.noreply.github.com>
2022-08-28 15:53:05 +0400

    Возьня с переборкой имён групп

--- Create Community VK/vk_create_group.py
+++ Create Community VK/vk_create_group.py
@@ -11,12 +11,35 @@ def create_group():
 
 def get_list_groups(var_user_id):
     list_groups = vk.groups.get(user_id=var_user_id, extended=1, filter='admin', fields='name')
-    print(list_groups['count'])
-    items_list_group = list_groups['items'][0]
-    dict_info_first_group = items_list_group['name']
-    print(dict_info_first_group)
-    print(items_list_group)
-    items_list_group = list_groups['items'][1]
-    print(items_list_group)
+    count_groups = list_groups['count']
+    # print(count_groups)
+    """
+    items_first_group = list_groups['items'][0]
+    name_first_group = items_first_group['name']
+    print(name_first_group)
+    print(items_first_group)
+    items_second_group = list_groups['items'][1]
+    print(items_second_group)
+    """
+    items_all_groups = list_groups['items']
+    # print(items_all_groups[1])
+    return count_groups, items_all_groups
 
-get_list_groups(user_id)
+
+def check_duplicate_group():
+    for i in range(len((values_groups[1]))):
+        items_first_group = values_groups['items'][1]
+        print(i, items_first_group['name'])
+    else:
+        return 1
+
+
+values_groups = get_list_groups(user_id)
+# [0] = count groups
+# [1] = items groups
+counts_groups = values_groups[0]
+items_groups = values_groups[1]
+# items_groups[1] значения первой группы
+items_first_group = items_groups[1]
+print(items_first_group['name'])
+
