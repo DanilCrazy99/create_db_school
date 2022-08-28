07a6b90534a34c38d680fcac1583adaf9f77fd90
Danil <volchara287@gmail.com>
2022-08-28 08:52:57 +0400

    Создал скрипт для создания группы. Необходимо ввести проверку на наличие такой группы(по названию).

--- Create Community VK/vk_create_group.py
+++ Create Community VK/vk_create_group.py
@@ -1,23 +1,12 @@
 import vk_api
-from Variables.Var_community import vk_token
-from Variables.Var_community import title, description, type
-token = vk_token
-vk_session = vk_api.VkApi(token=token)
-vk = vk_session.get_api()
+from Variables.Var_community import vk_token, title, description, type
 
-def main():
+session = vk_api.VkApi(token=vk_token)
+vk = session.get_api()
 
 
-
-    # vk_create_group = vk_session.method()
-    """
-    try:
-        vk_session.auth(token_only=True)
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return
-    """
+def create_group():
+    vk.groups.create(title=title, description=description, type=type)
 
 
-if __name__ == '__main__':
-    main()
+create_group()
