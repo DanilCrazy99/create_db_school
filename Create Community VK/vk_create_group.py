b5bb676708e35418135323867647693efbaf97b9
Danil <volchara287@gmail.com>
2022-08-28 08:30:24 +0400

    Пробую написать код для создания группы. В переменных создал строки title, description, type.

--- Create Community VK/vk_create_group.py
+++ Create Community VK/vk_create_group.py
@@ -1,30 +1,22 @@
-# -*- coding: utf-8 -*-
 import vk_api
-
+from Variables.Var_community import vk_token
+from Variables.Var_community import title, description, type
+token = vk_token
+vk_session = vk_api.VkApi(token=token)
+vk = vk_session.get_api()
 
 def main():
-    """ Пример получения последнего сообщения со стены """
 
-    login, password = '89511938669', 'D2XH4+_FpYVj_#2'
-    vk_session = vk_api.VkApi(login, password)
 
+
+    # vk_create_group = vk_session.method()
+    """
     try:
         vk_session.auth(token_only=True)
     except vk_api.AuthError as error_msg:
         print(error_msg)
         return
-
-    vk = vk_session.get_api()
-
-    """ VkApi.method позволяет выполнять запросы к API. В этом примере
-        используется метод wall.get (https://vk.com/dev/wall.get) с параметром
-        count = 1, т.е. мы получаем один последний пост со стены текущего
-        пользователя.
     """
-    response = vk.wall.get(count=1)  # Используем метод wall.get
-
-    if response['items']:
-        print(response['items'][0])
 
 
 if __name__ == '__main__':
