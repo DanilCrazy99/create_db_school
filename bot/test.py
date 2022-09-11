3a2a505d520686ec9e774433a59d6cc4fabcabeb
SergeyV70 <sergey@times-money.ru>
2022-09-11 23:28:57 +0400

    удалил файл test.py проверка бота.

--- bot/test.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from Variables.token import token
-
--- bot/vk_bot.py
+++ bot/vk_bot.py
@@ -8,6 +8,8 @@ import logging
 import traceback
 
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from Variables.token import token_group
+from Variables.Var_community import group_id
 
 
 class MyLongPoll(VkBotLongPoll):
@@ -23,7 +25,34 @@ class MyLongPoll(VkBotLongPoll):
 
 class VkBot:
     def __init__(self):
-        pass
+        # для longpoll
+        __api_token = token_group
+        __group_id = group_id
+
+        # подключаемся к VK
+        self.vk = vk_api.VkApi(token=__api_token)
+        # подключение к long_poll
+        self.long_poll = MyLongPoll(self.vk, __group_id)
+
+        # для использования методов VK
+        self.vk_api = self.vk.get_api()
 
     def start(self):
         logging.info('Запущен основной цикл бота')
+
+        try:
+            for event in self.long_poll.listen():
+                 print('входное сообщение: ', event)
+
+        except requests.exceptions.ReadTimeout:
+            error_msg = traceback.format_exc()
+            logging.error(f'ошибка подключения: {error_msg}')
+            time.sleep(10)
+
+        except Exception:
+            error_msg = traceback.format_exc()
+            # print(f'Произошла ошибка в файле бота:\n    {error_msg}\nПерезапуск...')
+            logging.info(f'источник краша...')
+            logging.error(f'Произошла ошибка в файле бота:{error_msg}')
+            logging.info('Перезапуск...')
+            time.sleep(5)
