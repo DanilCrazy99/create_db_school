7ed84fca4487ae57d54a63fe8120110b4c93abd6
Данил <112066653+DanilCrazy99@users.noreply.github.com>
2022-09-14 19:33:58 +0400

    create_postgres_list выдаёт лист готовый к отгрузке в Send_timetable.py

--- Create Database/Create_timetable.py
+++ Create Database/Create_timetable.py
@@ -22,29 +22,29 @@ for columns in range(3, sheet.max_column):  # переборка по коорд
 
                 for i in range(7):  # Записываем 7 строк под ним
                     if sheet[rows + i + 1][columns].value is None:  # Убираю значения None из списков
-                        list_lessons.append(f'{name_class}Y_')
+                        list_lessons.append(f'{name_class}, _')
                     else:
                         lesson_cell_coordinate_number = rows + i + 1
-                        list_lessons.append(str(name_class) +
-                                            'Y' +
+                        list_lessons.append(name_class +
+                                            ', ' +
                                             str(sheet[lesson_cell_coordinate_number][1].value)[0] +
-                                            'Y' +
-                                            str(sheet[rows + i + 1][columns].value) +
-                                            'Y' +
-                                            str(day_week)
+                                            ', ' +
+                                            sheet[rows + i + 1][columns].value +
+                                            ', ' +
+                                            day_week +
+                                            ', ' +
+                                            sheet[2][1].value
                                             )
                     if i == 6:
-                        list_lessons.append(f'{name_class}Y_')
-
-print(list_class)
-print(list_lessons)
+                        list_lessons.append(f'{name_class}, _')
 
 
-def create_postgres_list():
-    completed_list = [
-        [1, 2, 3, 4, 5, 6]]  # класс, урок(время), предмет, день недели, время(актуализации), ответственный
-    return completed_list  # Должно вернуть список который можно будет вставить в функцию INSERT
+def create_postgres_list(list_func):
+    completed_list = []
+    for a in range(len(list_func)):
+        if '_' not in list_func[a][4]:
+            completed_list.append(list_func[a])
+    return completed_list
 
-# for i in list_class['class']:
-#     if i == '5Б':
-#         print(list_class['class'].index(i))
+
+print(create_postgres_list(list_lessons))
