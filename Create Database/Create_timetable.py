96d714aadb61bf44682772acd62a8fc7e4f9146f
Данил <112066653+DanilCrazy99@users.noreply.github.com>
2022-09-14 16:38:32 +0400

    Черновик кода для закидывания расписания в postgres

--- Create Database/Create_timetable.py
+++ Create Database/Create_timetable.py
@@ -4,42 +4,38 @@ wb = openpyxl.load_workbook(path_timetable)
 sheet = wb.active
 # print(sheet[1][0].value)  # A1      ABCDEFGHI  INDEX[ЦИФРА][БУКВА]
 # print(sheet[3][4].value)  # E3      123456789
-list_class = {'class': [], 'lessons': []}
+list_class = []
 list_lessons = []
 
-for columns1 in range(3, sheet.max_column):
-    if sheet[3][columns1].value:
-        list_class['class'].append(sheet[3][columns1].value)
+for columns_name_class in range(3, sheet.max_column):  # Получаю список классов при помощи прохода по 3-й строке
+    if sheet[3][columns_name_class].value:
+        list_class.append(sheet[3][columns_name_class].value)
 
-for columns in range(3, sheet.max_column):
-    for rows in range(3, 52):
-        # print(sheet[rows][columns].value)
-        if sheet[rows][columns].value:
-            for a in (list_class['class']):
-                if a in sheet[rows][columns].value:
-                    for i in range(7):
-                        if sheet[rows + i + 1][columns].value is None:
+for columns in range(3, sheet.max_column):  # переборка по координате "буква"
+    for rows in range(3, 52):  # переборка по координате "цифра"
+        if sheet[rows][columns].value:  # Если выбранная ячейка содержит данные
+            for a in list_class:  # Переборка классов из листа list_class
+                if a in sheet[rows][columns].value:  # Если класс встретился при переборе таблицы
+                    for i in range(7):  # Записываем 7 строк под ним
+                        if sheet[rows + i + 1][columns].value is None:  # Убираю значения None из списков
                             list_lessons.append('_')
                         else:
                             list_lessons.append(sheet[rows + i + 1][columns].value)
                         if i == 6:
                             list_lessons.append('_')
 
-                    list_class['lessons'].append(list_lessons)
-                    list_lessons = []
-for i in range(6):
-    print(list_class['lessons'][i])
+
+print(list_class)
+print(list_lessons)
 
 
 def create_postgres_list():
 
     completed_list = [
         [1, 2, 3, 4, 5, 6]]  # класс, урок(время), предмет, день недели, время(актуализации), ответственный
-    return completed_list
+    return completed_list  # Должно вернуть список который можно будет вставить в функцию INSERT
 
 
-print(create_postgres_list())
-print(list_class)
 # for i in list_class['class']:
 #     if i == '5Б':
 #         print(list_class['class'].index(i))
