e793f4db7f3b51d4ab249946f46163c3025a4c82
Danil <volchara287@gmail.com>
2022-09-15 22:49:07 +0400

    Наконец то тот формат, который кушает postgres

--- Create_Database/Create_timetable.py
+++ Create_Database/Create_timetable.py
@@ -8,15 +8,15 @@ def create_timetable_list(path):
     # print(sheet[1][0].value)   A1    ABCDEFGHI  INDEX[ЦИФРА][БУКВА]
     # print(sheet[3][4].value)   E3    123456789
     list_class = []
-    list_lessons = []
+    list_lessons = [[]]
 
     for columns_name_class in range(3, sheet.max_column):  # Получаю список классов при помощи прохода по 3-й строке
         if sheet[3][columns_name_class].value:
             list_class.append(sheet[3][columns_name_class].value)
-
+    incr = 0
     for columns in range(3, sheet.max_column):  # переборка по координате "буква"
         for rows in range(3, 52):  # переборка по координате "цифра"
-            day_week_val = int(rows/8)
+            day_week_val = int(rows / 8)
             day_week = sheet[4 + day_week_val * 8][0].value
             if sheet[rows][columns].value:  # Если выбранная ячейка содержит данные
                 if sheet[rows][columns].value in list_class:  # Значение ячейки есть в списке с классами
@@ -24,21 +24,18 @@ def create_timetable_list(path):
 
                     for i in range(7):  # Записываем 7 строк под ним
                         if sheet[rows + i + 1][columns].value is None:  # Убираю значения None из списков
-                            list_lessons.append(f'{name_class}, _')
+                            list_lessons[incr].append(f'{name_class}, _')
                         else:
                             lesson_cell_coordinate_number = rows + i + 1
-                            list_lessons.append(name_class +
-                                                ', ' +
-                                                str(sheet[lesson_cell_coordinate_number][1].value)[0] +
-                                                ', ' +
-                                                sheet[rows + i + 1][columns].value +
-                                                ', ' +
-                                                day_week +
-                                                ', ' +
-                                                sheet[2][1].value
-                                                )
+                            list_lessons[incr].append(name_class)
+                            list_lessons[incr].append(str(sheet[lesson_cell_coordinate_number][1].value)[0])
+                            list_lessons[incr].append(sheet[rows + i + 1][columns].value)
+                            list_lessons[incr].append(day_week)
+                            list_lessons[incr].append(sheet[2][1].value)
                         if i == 6:
-                            list_lessons.append(f'{name_class}, _')
+                            list_lessons[incr].append(f'{name_class}, _')
+                        incr = +1
+                        list_lessons.append([])
     return list_lessons
 
 
@@ -49,8 +46,4 @@ def create_postgres_list(list_func):
             completed_list.append(list_func[a])
     return completed_list
 
-# К примеру было ['5А, 1, Русский, Понедельник, Петухова М.В', '5А, 2, Литература, Понедельник, Петухова М.В']
-# Стало [[5А], [1], [Русский], [Понедельник], [Петухова М.В], [5А], [2], [Литература], [Понедельник], [Петухова М.В]]
-
-
-print(create_timetable_list(path_timetable)[0][0])
+print(create_timetable_list(path_timetable)[0])
