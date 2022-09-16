fdf466eb0669a445c30da9948cc55b4c150cc697
SergeyV70 <sergey@times-money.ru>
2022-09-16 16:38:10 +0400

    добработка условия запроса выборки (database.py) select_timetable_class

--- Create_Community_VK/Bot/db/database.py
+++ Create_Community_VK/Bot/db/database.py
@@ -17,9 +17,18 @@ class DataBase:
         )
         self.__cursor = self.__connect.cursor()
 
-    def select_timetable_class(self, week_day, class_letter):
+    def select_timetable_class(self, week_day=[], class_letter=[]):
+        # формируем условия запроса по дню недели
+        sql_week = ''
+        sql_week += " OR ".join([f"day_of_week ='{item}'" for item in week_day])
+
+        # формируем условия запроса по классу
+        sql_class_letter = ''
+        sql_class_letter += " OR ".join([f"class ='{item}'" for item in class_letter])
+
         sql = "SELECT id, class, lesson_number, academic_discipline, day_of_week, editor, time_update " \
-              f"FROM timetable WHERE class='{class_letter}' AND day_of_week = '{week_day}';"
+              f"FROM timetable WHERE ({sql_class_letter}) AND ({sql_week});"
+        print('sql= ', sql)
         self.__cursor.execute(sql)
         result = self.__cursor.fetchall()
         return result
