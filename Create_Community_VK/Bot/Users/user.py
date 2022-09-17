2b7f5e0ed258503eb32193c468b44878615e57d8
Данил <112066653+DanilCrazy99@users.noreply.github.com>
2022-09-17 09:58:27 +0400

    Написал user.py для получения id участников беседы

--- Create_Community_VK/Bot/Users/user.py
+++ Create_Community_VK/Bot/Users/user.py
@@ -1,19 +1,26 @@
-import vk_api.vk_api
-
+import vk_api
 from Create_Community_VK.Config.Var_community import token_group
+from Create_Community_VK.Config.token import token
 
 
-class User:
-    def __init__(self, from_id=0):
-        self.__from_id = from_id
-        # подключаемся к VK
-        self.vk = vk_api.VkApi(token=self.__api_token)
-        # для использования методов VK
+class Community:
+    def __init__(self, token_in_class=token):
+        # присваиваю переменной token токен группы
+        # по дефолту равно токену из конфига
+        self.__token = token_in_class
+        self.vk = vk_api.VkApi(token=self.__token)
+        # используем .vk_api() для вызова API
         self.vk_api = self.vk.get_api()
+        # self.id_chat = ''
+        self.getConversation = self.vk_api.messages.getConversationMembers
 
-    def set_user_id(self, user_id):
-        self.__from_id = user_id
-        return self.__from_id
+    def get_members(self, id_chat):
+        list_items = self.getConversation(peer_id=id_chat)
+        list_members = []
+        for a in range(len(list_items['items'])):
+            list_members.append(list_items['items'][a]['member_id'])
+        return list_members
 
 
-
+test1 = Community()
+print(test1.get_members(2000000001))  # В скобки ввести id беседы
