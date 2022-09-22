1a7b6b28150117368847b5230dd5821f4113d822
Данил <112066653+DanilCrazy99@users.noreply.github.com>
2022-09-22 16:56:32 +0400

    добавил таблицу с временем активации расписания

--- Create_Database/Create_database.py
+++ Create_Database/Create_database.py
@@ -196,8 +196,15 @@ def db_main_tables(cursor):
         lesson_number integer NOT NULL, \
         academic_discipline text NOT NULL, \
         day_of_week text NOT NULL, \
-        editor text DEFAULT 'admin', \
-        time_update timestamp without time zone DEFAULT CURRENT_TIMESTAMP\
+        editor text DEFAULT 'admin'\
+        );"
+                   )
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS timetable_time_activate(\
+        id integer NOT NULL PRIMARY KEY, \
+        time_update timestamp without time zone DEFAULT CURRENT_TIMESTAMP, \
+        time_activate timestamp without time zone, \
+        id_timetable integer\
         );"
                    )
 
