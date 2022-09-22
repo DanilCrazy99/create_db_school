158394b74195518acf2ffbb155c6d0368f59dc77
Данил <112066653+DanilCrazy99@users.noreply.github.com>
2022-09-22 16:17:45 +0400

    небольшие коррективы

--- Create_Database/Create_database.py
+++ Create_Database/Create_database.py
@@ -113,37 +113,38 @@ def program_case(case_value):
 
 def db_main_tables(cursor):
     cursor.execute("CREATE TABLE IF NOT EXISTS academic_discipline(\
-        id bigserial NOT NULL PRIMARY KEY,\
-        name text NOT NULL,\
+        id bigserial NOT NULL PRIMARY KEY, \
+        name text NOT NULL, \
         room_id int NOT NULL DEFAULT 0\
         );"
                    )
 
     cursor.execute("CREATE TABLE IF NOT EXISTS add_education(\
-        id bigserial NOT NULL,\
-        name_education text NOT NULL PRIMARY KEY,\
+        id bigserial NOT NULL, \
+        name_education text NOT NULL PRIMARY KEY, \
         price money DEFAULT 0\
         );"
                    )
 
     cursor.execute("CREATE TABLE IF NOT EXISTS learner(\
-        id bigserial NOT NULL PRIMARY KEY,\
-        name text NOT NULL,\
-        surname text,\
-        phone integer\
+        id bigserial NOT NULL PRIMARY KEY, \
+        name text NOT NULL, \
+        surname text, \
+        phone integer, \
+        user_id integer\
         );"
                    )
 
     cursor.execute("CREATE TABLE IF NOT EXISTS role(\
-        id integer NOT NULL PRIMARY KEY UNIQUE,\
-        role text NOT NULL,\
+        id integer NOT NULL PRIMARY KEY UNIQUE, \
+        role text NOT NULL, \
         description text\
         );"
                    )
 
     cursor.execute("CREATE TABLE IF NOT EXISTS room_school(\
-        id bigserial NOT NULL PRIMARY KEY,\
-        liter_number text NOT NULL,\
+        id bigserial NOT NULL PRIMARY KEY, \
+        liter_number text NOT NULL, \
         discipline_id integer DEFAULT 0\
         );"
                    )
@@ -151,79 +152,85 @@ def db_main_tables(cursor):
     # Таблица student_parent/родители учеников (id, id ученика, имя, фамилия, отчество, телефон, доп информация)
     cursor.execute("CREATE TABLE IF NOT EXISTS student_parent(\
         id bigserial NOT NULL PRIMARY KEY, \
-        learner_id integer DEFAULT 0,\
+        learner_id integer DEFAULT 0, \
         name text NOT NULL, \
         surname text, \
         patronymic text, \
         phone integer, \
+        user_id integer, \
         description text\
     );"
                    )
 
     # Таблица teacher/учителя(id, имя, отчество, фамилия, телефон)
     cursor.execute("CREATE TABLE IF NOT EXISTS teacher(\
-         id bigserial NOT NULL PRIMARY KEY,\
-         name text NOT NULL,\
-         patronymic text,\
-         surname text,\
+         id bigserial NOT NULL PRIMARY KEY, \
+         name text NOT NULL, \
+         patronymic text, \
+         surname text, \
          phone integer\
          );"
                    )
 
     # Таблица vacation_schedule/отгулы,отпуски
     cursor.execute("CREATE TABLE IF NOT EXISTS vacation_schedule(\
-        id bigserial NOT NULL,\
-        teacher_id integer NOT NULL PRIMARY KEY,\
-        start_date date,\
-        stop_date date,\
+        id bigserial NOT NULL, \
+        teacher_id integer NOT NULL PRIMARY KEY, \
+        start_date date, \
+        stop_date date, \
         type_vacation_id integer\
         );"
                    )
 
     # Таблица vacation_type/тип отгулов,отпусков
     cursor.execute("CREATE TABLE IF NOT EXISTS vacation_type(\
-        id bigserial NOT NULL PRIMARY KEY,\
+        id bigserial NOT NULL PRIMARY KEY, \
         description text\
         );"
                    )
 
     # Таблица timetable/расписание
     cursor.execute("CREATE TABLE IF NOT EXISTS timetable(\
-        id bigserial NOT NULL PRIMARY KEY,\
-        class text NOT NULL,\
-        lesson_number integer NOT NULL,\
-        academic_discipline text NOT NULL,\
-        day_of_week text NOT NULL,\
-        editor text DEFAULT 'admin',\
-        time_update timestamp without time zone DEFAULT CURRENT_TIMESTAMP);"
+        id bigserial NOT NULL PRIMARY KEY, \
+        class text NOT NULL, \
+        lesson_number integer NOT NULL, \
+        academic_discipline text NOT NULL, \
+        day_of_week text NOT NULL, \
+        editor text DEFAULT 'admin', \
+        time_update timestamp without time zone DEFAULT CURRENT_TIMESTAMP\
+        );"
                    )
 
     # Таблица bot_messages/сообщения от бота
     cursor.execute("CREATE TABLE IF NOT EXISTS bot_messages(\
-        id bigserial NOT NULL PRIMARY KEY,\
-        msg text NOT NULL,\
-        id_user_vk integer NOT NULL,\
-        time timestamp without time zone DEFAULT CURRENT_TIMESTAMP);"
+        id bigserial NOT NULL PRIMARY KEY, \
+        msg text NOT NULL, \
+        id_user_vk integer NOT NULL, \
+        time timestamp without time zone DEFAULT CURRENT_TIMESTAMP\
+        );"
                    )
 
     # Таблица relations_class_id_chat/таблица зависимостей класса и id чатов
     cursor.execute("CREATE TABLE IF NOT EXISTS relations_class_id_chat(\
-            id bigserial NOT NULL PRIMARY KEY,\
-            class text NOT NULL,\
-            id_chat_vk integer NOT NULL);"
+            id bigserial NOT NULL PRIMARY KEY, \
+            class text NOT NULL, \
+            id_chat_vk integer NOT NULL\
+            );"
                    )
 
     # Таблица сервера состояний
-    cursor.execute("CREATE TABLE IF NOT EXISTS status_server("
-                   "id bigserial NOT NULL PRIMARY KEY, "
-                   "id_user_vk integer NOT NULL, "
-                   "id_status integer);"
+    cursor.execute("CREATE TABLE IF NOT EXISTS status_server(\
+            id bigserial NOT NULL PRIMARY KEY, \
+            id_user_vk integer NOT NULL, \
+            id_status integer\
+            );"
                    )
 
     # Таблица состояний
-    cursor.execute("CREATE TABLE IF NOT EXISTS status("
-                   "id bigserial NOT NULL PRIMARY KEY, "
-                   "status text NOT NULL);")
+    cursor.execute("CREATE TABLE IF NOT EXISTS status(\
+            id bigserial NOT NULL PRIMARY KEY, \
+            status text NOT NULL\
+            );")
 
 
 def db_not_main_tables(cursor):
