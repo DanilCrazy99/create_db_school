3ff07c0101d325c63dbe1b22a579c53f5941c1c8
SergeyV70 <sergey@times-money.ru>
2022-09-22 09:06:47 +0400

    запросы по работе с сервером статусов (database.py) insert_status_name / select_user_status_server / delete_status

--- Create_Community_VK/Bot/db/database.py
+++ Create_Community_VK/Bot/db/database.py
@@ -2,6 +2,7 @@
 
 # файл для работы с бд Postgresql
 import psycopg2
+import logging
 
 from Create_Database.Config.Var_database import schoolName, user, password, host, port
 
@@ -16,6 +17,19 @@ class DataBase:
                         port=port
         )
         self.__cursor = self.__connect.cursor()
+
+    @staticmethod
+    def format_args(sql, parameters: dict):
+        """
+        Создание параметра запроса для WHERE с условием AND
+        :param sql: начальная часть запроса с WHERE
+        :param parameters: именованный словарь с условиями. пример: {"поле_БД": переменная по которой будет выборка}
+        :return: строка SQL запроса и кортеж из параметров
+        """
+        sql += " AND ".join([
+            f"{item} = %s" for item in parameters
+        ])
+        return sql, tuple(parameters.values())
 
     def select_timetable_class(self, week_day=[], class_letter=[]):
         # формируем условия запроса по дню недели
@@ -34,7 +48,8 @@ class DataBase:
         result = self.__cursor.fetchall()
         return result
 
-    def select_roles_with_vk_id(self, id_user):  # Проверка на наличие специфичной роли в postgres
+    # Проверка на наличие специфичной роли в postgres
+    def select_roles_with_vk_id(self, id_user):
         with self.__cursor() as cursor:
             cursor.execute(
                 "SELECT * FROM role INNER JOIN users ON role.id = users.role_id;"
@@ -52,3 +67,32 @@ class DataBase:
                     if f['id'][i] == id_user:
                         return e['role'][i]
 
+    # добавление описателя статуса
+    def insert_status_name(self, status_name):
+        sql = "INSERT INTO status(status) VALUES (%s);"
+        self.__cursor.execute(sql, status_name)
+        self.__connect.commit()
+        logging.info(f'Добавлен новый статус {status_name}')
+
+    # получение текущего состояния юзера в сервере статусов
+    def select_user_status_server(self, user_id):
+        """
+        Получение текущего состояния юзера в сервере статуса
+        :param user_id: идентификатор пользователя в ВК
+        :return: возвращает описание статуса и id статуса
+        """
+        sql = "SELECT status_server.id_status, status.status " \
+              "FROM status_server INNER JOIN status ON status_server.id_status=status.id " \
+              f"WHERE status_server.id_user_vk={user_id};"
+        self.__cursor.execute(sql)
+        result = self.__cursor.fetchone()  # получение единичной записи
+        return result
+
+    # удаление описателя статуса
+    def delete_status(self, status_name):
+        # проверка использования статуса в таблице сервера состояний
+
+        sql = "DELETE FROM status WHERE id = %s;"
+        self.__cursor.execute(sql, status_name)
+        self.__connect.commit()
+        logging.info(f'Удаление статуса {status_name}')
