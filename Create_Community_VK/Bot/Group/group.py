6ad91a1a24cd82c582a428e7b72c2c643fa10eda
SergeyV70 <sergey@times-money.ru>
2022-09-30 03:35:40 +0400

    создал класс Group (group.py) работа с данными группы

---
+++ Create_Community_VK/Bot/Group/group.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import vk_api
+import requests
+from Create_Community_VK.Config.Var_community import token_group
+
+
+class Group:
+    """
+    Класс для работы с данными группы
+    """
+    def __init__(self, token_in_class=token_group):
+        # присваиваю переменной token токен группы
+        self.__token = token_in_class
+        self.vk = vk_api.VkApi(token=self.__token)
+        # используем .vk_api() для вызова API
+        self.vk_api = self.vk.get_api()
+        # self.id_chat = ''
+        self.getConversation_Mem = self.vk_api.messages.getConversationMembers
+        self.getChatList = self.vk_api.messages.getConversations
+        self.list_ids = []
+
+    def get_chats_ids(self):
+        """
+        Получаем ID чатов группы
+        :return: список ID чатов
+        """
+        list_items = self.getChatList()
+        list_ids = []
+        for a in range(len(list_items['items'])):
+            if list_items['items'][a]['conversation']['peer']['type'] == 'chat':
+                list_ids.append(list_items['items'][a]['conversation']['peer']['id'])
+        return list_ids
+
