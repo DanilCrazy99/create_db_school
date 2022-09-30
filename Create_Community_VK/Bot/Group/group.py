494369c8c20a3ac2ecb78fef269069d05a218ed9
SergeyV70 <sergey@times-money.ru>
2022-09-30 04:33:03 +0400

    заготовки для получения словарей с ссылками на чаты классов  (group.py) grouping_chats

--- Create_Community_VK/Bot/Group/group.py
+++ Create_Community_VK/Bot/Group/group.py
@@ -31,3 +31,21 @@ class Group:
                 list_ids.append(list_items['items'][a]['conversation']['peer']['id'])
         return list_ids
 
+    def get_chats_title(self):
+        """
+        Получение названий всех чатов группы
+        :return: список чатов классов
+        """
+        pass
+
+    def grouping_chats(self):
+        """
+        Группировка чатов в списки по критерию начальная/средняя школа
+        :return: словарь сгруппированных словарей чатов: уровень школы / поток классов
+        """
+        # Пример: {'primary': {1: {'A': 'link', 'B': 'link'}, 2:{}}, 'secondary': {5: {'A': 'link', 'B': 'link'}, 6:{}}}
+        # генерируем списки уровней школы, потоков классов и ссылки на литеры классов потока
+        level_school = dict.fromkeys(['primary', 'secondary'])
+        class_flow = dict.fromkeys(['flow', 'number'])
+        class_level = dict.fromkeys(['liter', 'linc'])
+        pass
