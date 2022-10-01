d6235e03dba25e5d036eb3a77d34e337e211e0cb
SergeyV70 <sergey@times-money.ru>
2022-10-01 06:21:37 +0400

    добавлены клавиатуры уровней (keyboard.py)

--- Create_Community_VK/Bot/keyboards/keyboard.py
+++ Create_Community_VK/Bot/keyboards/keyboard.py
@@ -3,6 +3,9 @@
 
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from Create_Community_VK.Bot.Users.user import Community
+from Create_Community_VK.Bot.Group.group import Group
+
+gr = Group()  # экземпляр класса Group
 
 
 def example_keyboard():
@@ -55,15 +58,38 @@ def generator_keyboard(role_id, one_time_method=False):
         keyboard.add_button('/старшие классы', color=VkKeyboardColor.POSITIVE)
         result = keyboard.get_keyboard()
 
-    elif role_id == 2:  # клавиатура главная для членов
-        keyboard.add_button('/вступить в чат', color=VkKeyboardColor.POSITIVE)
-        keyboard.add_line()  # Переход на новую строку
-        keyboard.add_button('/help', color=VkKeyboardColor.NEGATIVE)
-        keyboard.add_button('/покинуть чат', color=VkKeyboardColor.NEGATIVE)
+    elif role_id == 2:  # клавиатура выбора потока среди классов
+        # если это начальная школа, то одна строка с 4-мя клавами
+        # если средняя, то две
+        # генерировать клавы только для существующих потоков
+        primary_school = True
+        list_flow = gr.grouping_chats_level(primary_school=primary_school)
+        count_flow = len(list_flow)
+        if count_flow != 0:
+            if count_flow > 4:
+                step = 0
+                for flow in list_flow:
+                    step += 1
+                    if step == 4:
+                        keyboard.add_line()  # Переход на новую строку
+                    caption_key = '/' + flow
+                    keyboard.add_button(caption_key, color=VkKeyboardColor.POSITIVE)
 
         result = keyboard.get_keyboard()
 
-    elif role_id == 3:  # клавиатура выбора дня недели расписания
+    elif role_id == 3:  # клавиатура внутри потока и ссылками на чаты
+        list_litter_class = gr.litter_level_chat()
+        step = 0
+        if list_litter_class:
+            step += 1
+            if step == 4:
+                keyboard.add_line()  # Переход на новую строку
+            caption_key = '/' + list_litter_class[2]
+            keyboard.add_button(caption_key, color=VkKeyboardColor.POSITIVE)
+
+        result = keyboard.get_keyboard()
+
+    elif role_id == 4:  # клавиатура выбора дня недели расписания
         keyboard.add_button('/пн', color=color_key())
         keyboard.add_button('/вт', color=color_key())
         keyboard.add_button('/ср', color=color_key())
@@ -83,7 +109,15 @@ def generator_keyboard(role_id, one_time_method=False):
 
         result = keyboard.get_keyboard()
 
-    elif role_id == 4:  # клавиатура завуча
+    elif role_id == 5:  # клавиатура управления участием в чатах
+        keyboard.add_button('/вступить в чат', color=VkKeyboardColor.POSITIVE)
+        keyboard.add_line()  # Переход на новую строку
+        keyboard.add_button('/help', color=VkKeyboardColor.NEGATIVE)
+        keyboard.add_button('/покинуть чат', color=VkKeyboardColor.NEGATIVE)
+
+        result = keyboard.get_keyboard()
+
+    elif role_id == 6:  # клавиатура завуча
         keyboard.add_button('/загрузить расписание', color=VkKeyboardColor.POSITIVE)
         keyboard.add_button('/активировать расписание', color=VkKeyboardColor.POSITIVE)
         keyboard.add_line()  # Переход на новую строку
