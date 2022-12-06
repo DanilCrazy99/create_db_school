9dc8223c29a0ec13c214dfe9921ba030f330fa29
SergeyV70 <sergey@times-money.ru>
2022-12-06 15:50:04 +0400

    переписан метод загрузки файла расписаний в таблицы БД (Send_timetable.py) Добавление Id_timetable_activate в таблицу timetable

--- Create_Database/Send_timetable.py
+++ Create_Database/Send_timetable.py
@@ -35,39 +35,57 @@ def send_timetable(editor_id_vk, timetable_list, data_sheet=None):
         send_trigger = False
         if not completed_list:
             return False
-        for a in range(len(completed_list)):
-            without_brackets = str(completed_list[a])
-            without_brackets = without_brackets[:len(without_brackets)-1]
-            without_brackets = without_brackets[1:]
-            with connection.cursor() as cursor:
-                if not send_trigger:
-                    send_trigger = True
-                    cursor.execute("INSERT INTO timetable (class, "
-                                   "lesson_number, "
-                                   "academic_discipline, "
-                                   "day_of_week) "
-                                   f"VALUES ({without_brackets}) "
-                                   "RETURNING id"
-                                   )
-                    id_first_timetable = str(cursor.fetchall())
-                    id_first_timetable = id_first_timetable[2:-3]
-                else:
-                    cursor.execute("INSERT INTO timetable (class, "
-                                   "lesson_number, "
-                                   "academic_discipline, "
-                                   "day_of_week) "
-                                   f"VALUES ({without_brackets}) "
-                                   )
+
         with connection.cursor() as cursor:
             if data_sheet is None:
                 data_sheet = datetime.today()
             cursor.execute("INSERT INTO timetable_time_activate ("
                            "time_activate, "
                            "editor_user_id, "
-                           "class_flow, "
-                           "id_timetable) "
-                           f"VALUES ('{data_sheet}', '{str(editor_id_vk)}', '{str(completed_list[0][0][:-1])}', {id_first_timetable})"
+                           "class_flow) "
+                           f"VALUES ('{data_sheet}', '{str(editor_id_vk)}', '{str(completed_list[0][0][:-1])}') "
+                           "RETURNING id;"
                            )
+            id_timetable = cursor.fetchone()[0]
+            connection.commit()
+
+        for a in range(len(completed_list)):
+            without_brackets = str(completed_list[a])
+            without_brackets = without_brackets[:len(without_brackets)-1]
+            without_brackets = without_brackets[1:]
+            without_brackets += ', ' + str(id_timetable)
+            with connection.cursor() as cursor:
+                if not send_trigger:
+                    send_trigger = True
+                    cursor.execute("INSERT INTO timetable (class, "
+                                   "lesson_number, "
+                                   "academic_discipline, "
+                                   "day_of_week, "
+                                   "id_timetable) "
+                                   f"VALUES ({without_brackets})"
+                                   )
+                    # id_first_timetable = str(cursor.fetchall())
+                    # id_first_timetable = id_first_timetable[2:-3]
+                else:
+                    cursor.execute("INSERT INTO timetable (class, "
+                                   "lesson_number, "
+                                   "academic_discipline, "
+                                   "day_of_week, "
+                                   "id_timetable )"
+                                   f"VALUES ({without_brackets}) "
+                                   )
+
+        # with connection.cursor() as cursor:
+        #     if data_sheet is None:
+        #         data_sheet = datetime.today()
+        #     cursor.execute("INSERT INTO timetable_time_activate ("
+        #                    "time_activate, "
+        #                    "editor_user_id, "
+        #                    "class_flow, "
+        #                    "id_timetable) "
+        #                    f"VALUES ('{data_sheet}', '{str(editor_id_vk)}', '{str(completed_list[0][0][:-1])}', {id_first_timetable})"
+        #                    )
+
             connection.commit()
     except Exception as _ex:
         print("[INFO] Error while working with PostgresQL", _ex)
