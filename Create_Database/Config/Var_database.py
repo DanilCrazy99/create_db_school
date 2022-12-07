18798e6e1463d1ca652cf331fc12db1b4500132c
SergeyV70 <sergey@times-money.ru>
2022-12-07 20:12:43 +0400

    Добавлены данные для заполнения таблиц дефолтными установками.(Var_databse.py) default_data_role, default_data_status

--- Create_Database/Config/Var_database.py
+++ Create_Database/Config/Var_database.py
@@ -18,3 +18,12 @@ port = 5432
 class_school = '1A'
 
 path_timetable = r'Config/CopyTimetable.xlsx'
+
+default_data_role = ['user', 'creator', 'editor', 'visitor', 'administrator', 'editor_time_table']
+default_data_status = [['начало диалога', 1],
+                       ['потоки младших классов', 2],
+                       ['выбор ссылки на чат класса', 3],
+                       ['получение расписания', 4],
+                       ['потоки старших классов', 21],
+                       ['InLine клавиатура', 41]
+                       ]
